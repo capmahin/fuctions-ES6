18761a3b87c1828aca0ef4ea7eb554cd0ef74b3f
capmahin <mahinulhuque1996@gmail.com>
2022-02-19 17:33:58 +0600

    fuctions

---
+++ fuctions.js
@@ -0,0 +1,18 @@
+// expretions 
+const add = function add(num1,num2){
+    return num1 + num2;
+
+}
+const sum = add(12,14);
+console.log(sum);
+// anonymas fuictions
+const add1= function (num1,num2){
+    return num1 + num2;
+
+}
+const sum1 = add1(12,14);
+console.log(sum1);
+//arrow fuction
+const add2 = (num1,num2) => num1 + num2;
+const sum3 = add2(12,14);
+console.log(sum3);
